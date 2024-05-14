00a7dc94a3ee65bc2033b6f009515bcae8c459f8
Victor Notaro <victorhugonotaro@icloud.com>
2024-05-13 23:32:52 -0300

    Mudança no Components

--- src/App.vue
+++ src/App.vue
@@ -1,50 +1,45 @@
-<script setup>
-import {ref} from 'vue'
-
-let num = ref(0)
-
-</script>
 <template>
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body">
-  <div class="container-fluid">
-    <router-Link class="navbar-brand" to="/"><h1 class="logo">VIMA<span>STORE</span></h1></router-Link>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <router-Link class="nav-link" aria-current="page" to="/">Inicio</router-Link>
-        </li>
-        <li class="nav-item">
-          <router-Link class="nav-link" aria-current="page" to="/about">Page</router-Link>
-        </li>
-        <li class="nav-item">
-          <router-Link class="nav-link" aria-current="page" to="/cart">
-            <span class="material-symbols-outlined">shopping_cart</span>
-          </router-Link>
-        </li>
-        
-        <li class="nav-item" >
-          <router-Link class="nav-link" aria-current="page" to="/about">
-            <span class="material-symbols-outlined">account_circle</span>
-          </router-Link>
-        </li>
-      
-      </ul>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark border-bottom border-body">
+    <div class="container-fluid">
+      <router-Link class="navbar-brand" to="/"><h1 class="logo">VIMA<span>STORE</span></h1></router-Link>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <router-Link class="nav-link" aria-current="page" to="/">Inicio</router-Link>
+          </li>
+          <li class="nav-item">
+            <router-Link class="nav-link" aria-current="page" to="/about">Page</router-Link>
+          </li>
+          <li class="nav-item">
+            <router-Link class="nav-link" aria-current="page" to="/cart">
+              <span class="material-symbols-outlined">shopping_cart</span>
+            </router-Link>
+          </li>
+          <li class="nav-item">
+            <router-Link class="nav-link" aria-current="page" to="/about">
+              <span class="material-symbols-outlined">account_circle</span>
+            </router-Link>
+          </li>
+        </ul>
+      </div>
     </div>
+  </nav>
+  <div class="main">
+    <router-view></router-view>
   </div>
-</nav>
-<div class="main">
-  <router-view></router-view>
-</div>
-
-
-
 </template>
+
+<script setup>
+import { ref } from 'vue';
+
+let num = ref(0);
+</script>
+
 <style>
-footer{
+footer {
   position: fixed;
   bottom: 0;
   width: 100%;
@@ -54,28 +49,25 @@ footer{
   height: 50px;
 }
 
-.logo{
-    display: flex;
-    align-items: center;
-    color: var(--tex-color);
-    font-size: 28px;
-    font-weight: bold;
+.logo {
+  display: flex;
+  align-items: center;
+  color: var(--tex-color);
+  font-size: 28px;
+  font-weight: bold;
 }
 
-.logo i{
-    color: var(--main-color);
-    font-size: 32px;
-    margin-right: 5px;
+.logo i {
+  color: var(--main-color);
+  font-size: 32px;
+  margin-right: 5px;
 }
 
-.logo{
-    color: #34e7f8;
+.logo {
+  color: #34e7f8;
 }
 
-.logo span{
-    color: #fff;
+.logo span {
+  color: #fff;
 }
-
-
-
 </style>
